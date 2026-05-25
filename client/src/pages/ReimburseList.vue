a0fdc9b928e10375215f26fc0e8ca9a3dfa314eb
ecobaier <2783363899@qq.com>
2026-05-26 00:13:39 +0800

    Delete client directory

--- client/src/pages/ReimburseList.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <main class="page-shell p-5">
-    <section class="prototype-card min-h-[520px] p-6">
-      <div class="flex h-[420px] items-center justify-center text-gray-500">
-        报销单列表页面骨架已就绪，下一阶段接入静态数据与 Mock 模型。
-      </div>
-    </section>
-  </main>
-</template>
